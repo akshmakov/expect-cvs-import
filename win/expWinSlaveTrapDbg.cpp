d751c97693a0fa26309c4e19726ff3ded88392ae
davygrvy <>
2002-03-12 07:59:14 +0000

    final cleaning of Tcl from the slavedrv.

--- win/expWinSlaveTrapDbg.cpp
+++ win/expWinSlaveTrapDbg.cpp
@@ -29,11 +29,11 @@
  *	    http://expect.sf.net/
  *	    http://bmrc.berkeley.edu/people/chaffee/expectnt.html
  * ----------------------------------------------------------------------------
- * RCS: @(#) $Id: expWinSlaveTrapDbg.cpp,v 1.1.4.5 2002-03-12 07:09:36 davygrvy Exp $
+ * RCS: @(#) $Id: expWinSlaveTrapDbg.cpp,v 1.1.4.6 2002-03-12 07:59:14 davygrvy Exp $
  * ----------------------------------------------------------------------------
  */
 
-#include "expWinInt.h"
+#include "expWinSlave.hpp"
 
 
 ExpSlaveTrapDbg::ExpSlaveTrapDbg(int argc, char * const argv[], CMclQueue<Message *> &_mQ)
